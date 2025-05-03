927f6cf8c1e5f571d190253957f90b18623dd8b1
Nguyễn Hoàng Anh <135218445+Hoanganh999@users.noreply.github.com>
2025-05-03 12:09:43 +0700

    Update sections_movies_item.blade.php

--- resources/views/themevlxx/inc/sections_movies_item.blade.php
+++ resources/views/themevlxx/inc/sections_movies_item.blade.php
@@ -4,7 +4,7 @@
         <img class="video-image lazyload" src="{{$movie->getPosterUrl()}}"
     data-original="{{$movie->getPosterUrl()}}"
     alt="{{$movie->name}}"
-    style="width: 150px; height: 90px; object-fit: cover; display: block;">
+    style="width: 200px; height: 1200px; object-fit: cover; display: block;">
         <div class="ribbon">{{$movie->language}}</div>
     </a>
     <div class="video-name">
