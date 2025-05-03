b066cb7db3d9a50493a36b61fcc25b498ba64e9c
Nguyễn Hoàng Anh <135218445+Hoanganh999@users.noreply.github.com>
2025-05-03 13:47:18 +0700

    Update episode.blade.php

--- resources/views/themevlxx/episode.blade.php
+++ resources/views/themevlxx/episode.blade.php
@@ -62,8 +62,9 @@
                     <a title="{{$movie->name}}"
                         href="{{$movie->getUrl()}}">
                         <img class="video-image lazyload" src="{{$movie->getPosterUrl()}}"
-                            data-original="{{$movie->getPosterUrl()}}" width="240px" height="180px"
-                            alt="{{$movie->name}}" style="">
+    data-original="{{$movie->getPosterUrl()}}"
+    alt="{{$movie->name}}"
+    style="width: 178px; height: 107px; object-fit: cover; display: block;">
                         <div class="ribbon">{{$movie->language}}</div>
                     </a>
                     <div class="video-name">
