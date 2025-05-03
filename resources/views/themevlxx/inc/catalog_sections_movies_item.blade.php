69ef052c9a8455c36a3425d4b0500ebe3e109516
Nguyễn Hoàng Anh <135218445+Hoanganh999@users.noreply.github.com>
2025-05-03 13:02:25 +0700

    Update catalog_sections_movies_item.blade.php

--- resources/views/themevlxx/inc/catalog_sections_movies_item.blade.php
+++ resources/views/themevlxx/inc/catalog_sections_movies_item.blade.php
@@ -4,7 +4,7 @@
         <img class="video-image lazyload" src="{{$movie->getPosterUrl()}}"
     data-original="{{$movie->getPosterUrl()}}"
     alt="{{$movie->name}}"
-    style="width: 200px; height: 120px; object-fit: cover; display: block;">
+    style="width: 240px; height: 150px; object-fit: cover; display: block;">
         <div class="ribbon">Vietsub</div>
     </a>
     <div class="video-name">
